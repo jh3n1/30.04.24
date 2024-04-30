6c4455810b846bba7cae55814df2aac4b22f4885
jh3n1 <142435558+jh3n1@users.noreply.github.com>
2024-04-30 11:28:48 -0300

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,16 @@
+Class Object {
+   constructor (parametro1, parametro2) {
+    this.pinterest = pinterest;
+    this.pinterest2 = pinterest2;
+   }
+
+   metodo1 () {
+    window.open(this.site);
+    window.open(this.pinterest);
+   }
+
+   metodo1 () {
+    window.open(this.site);
+    window.open(this.pinterest2);
+   }
+}
